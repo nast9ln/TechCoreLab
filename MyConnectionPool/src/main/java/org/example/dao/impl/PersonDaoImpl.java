827024e98d8b7a6bb1ad0ccf2357e4c237b7ba76
nastya <nast9ln@vk.com>
2024-06-03 13:47:22 +0300

    took out the default scheme

--- MyConnectionPool/src/main/java/org/example/dao/impl/PersonDaoImpl.java
+++ MyConnectionPool/src/main/java/org/example/dao/impl/PersonDaoImpl.java
@@ -6,16 +6,19 @@ import org.example.entity.Person;
 import org.example.exception.EntityNotFoundException;
 import org.example.exception.LoginDuplicateException;
 import org.example.util.ConnectionManager;
+import org.example.util.PropertiesUtil;
 
 import java.sql.*;
+import java.text.MessageFormat;
 import java.time.ZoneId;
 import java.util.Optional;
 
 public class PersonDaoImpl implements PersonDao {
-    private static final String INSERT_PERSON = "insert into techcore.person (name, registration_date, login, role) values (?, ?, ?, ?)";
-    private static final String UPDATE_PERSON = "update techcore.person set name= ?, registration_date = ?, login = ?, role = ? WHERE id = ?";
-    private static final String DELETE_PERSON = "delete from techcore.person where id = ?";
-    private static final String FIND_BY_ID = "SELECT id, name, registration_date, login, role FROM techcore.person WHERE id = ? ";
+    private static final String DEFAULT_SCHEMA = "db.schema";
+    private static final String INSERT_PERSON = "INSERT INTO {0}.person (name, registration_date, login, role) VALUES (?, ?, ?, ?)";
+    private static final String UPDATE_PERSON = "UPDATE {0}.person SET name= ?, registration_date = ?, login = ?, role = ? WHERE id = ?";
+    private static final String DELETE_PERSON = "DELETE FROM {0}.person WHERE id = ?";
+    private static final String FIND_BY_ID = "SELECT id, name, registration_date, login, role FROM {0}.person WHERE id = ? ";
     private final RoleDao roleDao;
 
     public PersonDaoImpl(RoleDao roleDao) {
@@ -25,7 +28,7 @@ public class PersonDaoImpl implements PersonDao {
     @Override
     public Person save(Person person) {
         try (Connection connection = ConnectionManager.get();
-             PreparedStatement statement = connection.prepareStatement(INSERT_PERSON, Statement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement statement = connection.prepareStatement(MessageFormat.format(INSERT_PERSON, PropertiesUtil.get(DEFAULT_SCHEMA)), Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, person.getName());
             statement.setDate(2, Date.valueOf(person.getRegistrationDate().atZone(ZoneId.systemDefault()).toLocalDate()));
             statement.setString(3, person.getLogin());
@@ -48,7 +51,7 @@ public class PersonDaoImpl implements PersonDao {
     @Override
     public Person update(Person person) {
         try (Connection connection = ConnectionManager.get();
-             PreparedStatement statement = connection.prepareStatement(UPDATE_PERSON)) {
+             PreparedStatement statement = connection.prepareStatement(MessageFormat.format(UPDATE_PERSON, PropertiesUtil.get(DEFAULT_SCHEMA)))) {
             statement.setString(1, person.getName());
             statement.setTimestamp(2, Timestamp.from(person.getRegistrationDate()));
             statement.setString(3, person.getLogin());
@@ -64,7 +67,7 @@ public class PersonDaoImpl implements PersonDao {
     @Override
     public void delete(Long id) {
         try (Connection connection = ConnectionManager.get();
-             PreparedStatement statement = connection.prepareStatement(DELETE_PERSON)) {
+             PreparedStatement statement = connection.prepareStatement(MessageFormat.format(DELETE_PERSON, PropertiesUtil.get(DEFAULT_SCHEMA)))) {
             statement.setLong(1, id);
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -75,7 +78,7 @@ public class PersonDaoImpl implements PersonDao {
     @Override
     public Optional<Person> findById(Long id) {
         try (Connection connection = ConnectionManager.get();
-             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(MessageFormat.format(FIND_BY_ID, PropertiesUtil.get(DEFAULT_SCHEMA)))) {
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
